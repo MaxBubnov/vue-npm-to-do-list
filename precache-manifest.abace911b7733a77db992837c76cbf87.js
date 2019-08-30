f1cf9db49bce7a26e1358709d38b674a89eb6da0
Maxim <m.bubnov05@gmail.com>
2019-08-30 20:43:26 +0300

    'Deploy'

---
+++ precache-manifest.abace911b7733a77db992837c76cbf87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0d41151458d62d50d698",
+    "url": "/js/chunk-vendors.ccfe9640.js"
+  },
+  {
+    "revision": "7ab2baf1c22ed6a9d589",
+    "url": "/js/app.7c0078b0.js"
+  },
+  {
+    "revision": "7b0fb69fe86091bb4b455f9f7ee6dbe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ab2baf1c22ed6a9d589",
+    "url": "/css/app.19991f7c.css"
+  }
+];
